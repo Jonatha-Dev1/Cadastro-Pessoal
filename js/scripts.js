7a9ecf6f975ac04b7b4f57772b14c5b018abee5d
Jonatha Teixeira <silva.jonatha@outlook.com>
2021-06-02 11:22:54 -0300

    aqruivo modificado index.html e diretorios e arquivos criados cc/styles.css img/programacao.jpg js/script.js

---
+++ js/scripts.js
@@ -0,0 +1,89 @@
+let submit = document.getElementById('btn-submit')
+
+submit.addEventListener('click', function alertSubmit(){
+    alert('Obrigado! Seu cadastro foi realizado com sucesso.')
+})
+
+
+
+
+
+
+
+/*class Validator {
+
+  constructor() {
+    this.validations = [
+      'data-required',
+    ]
+  }
+
+  
+  validate(form) {
+
+    let inputs = form.getElementsByTagName('input');
+    
+    let inputsArray = [...inputs];
+    
+    inputsArray.forEach(function(input, obj) {
+
+      for(let i = 0; this.validations.length > i; i++) {
+        if(input.getAttribute(this.validations[i]) != null) {
+
+          let method = this.validations[i].replace("data-", "").replace("-", "");
+
+          let value = input.getAttribute(this.validations[i])
+
+          this[method](input,value);
+
+        }
+      }
+
+    }, this);
+
+  }
+  
+  required(input) {
+
+    let inputValue = input.value;
+
+    if(inputValue === '') {
+      let errorMessage = `Este campo é obrigatório`;
+
+      this.printMessage(input, errorMessage);
+    }
+
+  }
+
+ 
+  printMessage(input, msg) {
+  
+    let errorsQty = input.parentNode.querySelector('.error-validation');
+
+    if(errorsQty === null) {
+      let template = document.querySelector('.error-validation').cloneNode(true);
+
+      template.textContent = msg;
+  
+      let inputParent = input.parentNode;
+  
+      template.classList.remove('template');
+  
+      inputParent.appendChild(template);
+    }
+
+  }
+
+}
+
+let form = document.getElementById('register-form');
+let submit = document.getElementById('btn-submit');
+
+
+let validator = new Validator();
+
+submit.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  validator.validate(form);
+});*/
